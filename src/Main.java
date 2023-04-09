85e41f1ee99c1ddf85c2902444e44744a3e60b2d
Sadagat Asgarov <sadagatasgarov@gmail.com>
2023-04-09 11:59:54 -0700

    matris

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,50 @@
-import java.lang.reflect.Array;
-
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+        String[][] sehirler = new String[3][3];
+        sehirler[0][0]= "Istanbul";
+        sehirler[0][1]= "Adana";
+        sehirler[0][2]= "Baki";
+        sehirler[1][0]= "Sumqayit";
+        sehirler[1][1]= "Izmir";
+        sehirler[1][2]= "Marmara";
+        sehirler[2][0]= "Naxcivan";
+        sehirler[2][1]= "Ordubad";
+        sehirler[2][2]= "Culfa";
+
+        for (int i = 0; i< sehirler.length; i++){
+            for (int j=0; j< sehirler.length; j++){
+                System.out.println(sehirler[i][j]);
+            }
+        }
+
+/*
+        double[] myList = {1,2,3,4,5,6,7};
+        double total = 0;
+        double max = myList[0];
+        for (double number:myList){
+            total=total+number;
+            System.out.println(number);
+        }
+        System.out.println("Toplam = "+total);
+*/
+/*
         String[] ogrencier = new String[4];
         ogrencier[0]="Ahmet";
         ogrencier[1]="Salih";
         ogrencier[2]="aaa";
-        ogrencier[3]="dsdh";/*
+        ogrencier[3]="dsdh";
+        */
+/*
         for (int i = 0; i < ogrencier.length; i++) {
             System.out.println(ogrencier[i]);
-        }*/
+        }*//*
+
         for (String ogrenci:ogrencier) {
             System.out.println(ogrenci);
         }
-
-
-
+*/
 /*
         //for
         for(int i=0; i<10; i+=2){
@@ -26,8 +53,7 @@ public class Main {
 
         System.out.println("Dongu bitti");
 */
-
-        int i = 1;
+     /*   int i = 1;*/
 /*
         while (i < 10) {
             System.out.println(i);
@@ -35,12 +61,10 @@ public class Main {
         }
         System.out.println("Dongu bitti");
 */
-
 /*        do{
             System.out.println(i);
             i++;
         }while (i<10);*/
-
 /*
         int sayi = 10;
         String sehir = "Ankara";
@@ -80,8 +104,5 @@ public class Main {
                 System.out.println("Kece bilmediniz");
         }
 */
-
-
-
     }
 }
