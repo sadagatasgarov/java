72e817a65cb3c73b06ebf70bf55e44621e75060f
Sadagat Asgarov <sadagatasgarov@gmail.com>
2023-04-10 20:42:42 +0400

    aa

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,11 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        int[] sayilar = new int[]{1,2,3,4,5,7,8,9,0};
+        sayiBulmaca();
+    }
+
+    public static void sayiBulmaca(){
+        int[] sayilar = new int[]{1,2,3,4,5,6,7,8,9,0};
         int aranacak = 6;
         boolean VarMi=false;
         for (int sayi :
@@ -15,10 +19,13 @@ public class Main {
 
         if (VarMi) {
             System.out.println("Sayi var");
+            mesajVer(aranacak);
         }else {
             System.out.println("Sayi yoxdur");
         }
+    }
 
-
+    public static void mesajVer(int aranacak){
+        System.out.println("Sayi ,movcuddur: " + aranacak);
     }
 }
