d8ddceaa3a9329147f08f73789c94ee05b7c2e51
Sadagat Asgarov <sadagatasgarov@gmail.com>
2023-04-10 21:14:28 +0400

    topla

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+        String mesaj = "Bugun hava cok gozel";
+        String yeniMesaj = mesaj.substring(0,2);
+        System.out.println(topla(2,4));
 
     }
     public static void ekle(){
@@ -10,10 +13,28 @@ public class Main {
     }
 
     public static void sil(){
-
+        System.out.println("Silindi");
     }
 
     public static void guncelle(){
+        System.out.println("Guncellendi");
+    }
 
+/*
+    public static double topla(int i, int b){
+        int c;
+        c=i+b;
+        return c;
+    }
+*/
+
+
+    public static int topla(int... sayilar){
+        int topla=0;
+        for (int sayi:sayilar
+             ) {
+            topla = topla+sayi;
+        }
+        return topla;
     }
 }
